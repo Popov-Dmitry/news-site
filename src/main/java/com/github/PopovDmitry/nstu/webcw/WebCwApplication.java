c4ec0fc4c8d8fab4489fe1f5d76399f29419f95f
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-04-07 19:43:25 +0700

    [STUDY-69] Init commit

---
+++ src/main/java/com/github/PopovDmitry/nstu/webcw/WebCwApplication.java
@@ -0,0 +1,13 @@
+package com.github.PopovDmitry.nstu.webcw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/PopovDmitry/nstu/webcw/WebCwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.PopovDmitry.nstu.webcw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
