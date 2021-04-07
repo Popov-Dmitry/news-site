57605e54eaaeca99574d5b45ea5945145fe6d4df
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-04-07 19:59:20 +0700

    [STUDY-69] Fixed lombok version for JDK 16

--- src/main/java/com/github/PopovDmitry/nstu/webcw/WebCwApplication.java
+++ src/main/java/com/github/PopovDmitry/nstu/webcw/WebCwApplication.java
@@ -2,8 +2,9 @@ package com.github.PopovDmitry.nstu.webcw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class WebCwApplication {
 
 	public static void main(String[] args) {
