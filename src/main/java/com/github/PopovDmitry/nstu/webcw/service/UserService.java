07ef8667a9c3446b9cb180a9dc663810099e7eec
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-04-08 18:23:08 +0700

    [STUDY-69] Added UserService

---
+++ src/main/java/com/github/PopovDmitry/nstu/webcw/service/UserService.java
@@ -0,0 +1,35 @@
+package com.github.PopovDmitry.nstu.webcw.service;
+
+import com.github.PopovDmitry.nstu.webcw.model.User;
+import com.github.PopovDmitry.nstu.webcw.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public Optional<User> getUser(long id) {
+        return userRepository.findById(id);
+    }
+
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+
+    public void deleteUser(User user) {
+        userRepository.delete(user);
+    }
+}
