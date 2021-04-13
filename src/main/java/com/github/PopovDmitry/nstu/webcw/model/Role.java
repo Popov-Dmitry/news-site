c141b6a9703d50fcc549a0a29e29c663c11051bd
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-04-13 23:32:56 +0700

    [STUDY-69] Added permissions

--- src/main/java/com/github/PopovDmitry/nstu/webcw/model/Role.java
+++ src/main/java/com/github/PopovDmitry/nstu/webcw/model/Role.java
@@ -1,7 +1,28 @@
 package com.github.PopovDmitry.nstu.webcw.model;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public enum Role {
-    ADMIN,
-    MODERATOR,
-    USER
+    ADMIN(Set.of(Permission.ARTICLES_READ, Permission.ARTICLES_WRITE)),
+    MODERATOR(Set.of(Permission.ARTICLES_READ, Permission.ARTICLES_WRITE)),
+    USER(Set.of(Permission.ARTICLES_READ, Permission.ARTICLES_WRITE));
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return permissions.stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+    }
 }
