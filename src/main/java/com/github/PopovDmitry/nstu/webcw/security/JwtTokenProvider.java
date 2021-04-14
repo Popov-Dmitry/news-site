7186a308d2b71906e4b3ac3d1044fe8b0ebe10a8
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-04-14 16:52:29 +0700

    [STUDY-69] Added JwtTokenProvider

---
+++ src/main/java/com/github/PopovDmitry/nstu/webcw/security/JwtTokenProvider.java
@@ -0,0 +1,39 @@
+package com.github.PopovDmitry.nstu.webcw.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long validityInMilliseconds;
+
+    public String createToken(String email, String role) {
+        Claims claims = Jwts.claims().setSubject(email);
+        claims.put("role", role);
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds * 1000);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public boolean validate(String token) {
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+        return !claimsJws.getBody().getExpiration().before(new Date());
+    }
+}
