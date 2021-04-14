3d3c58f7d6a811bc5fd1ae3cc535b0f186713f27
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-04-15 02:32:32 +0700

    [STUDY-69] Added SecurityConfig

--- src/main/java/com/github/PopovDmitry/nstu/webcw/WebCwApplication.java
+++ src/main/java/com/github/PopovDmitry/nstu/webcw/WebCwApplication.java
@@ -2,9 +2,8 @@ package com.github.PopovDmitry.nstu.webcw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class WebCwApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/github/PopovDmitry/nstu/webcw/controller/rest/AuthenticationRestController.java
+++ src/main/java/com/github/PopovDmitry/nstu/webcw/controller/rest/AuthenticationRestController.java
@@ -1,7 +1,15 @@
 package com.github.PopovDmitry.nstu.webcw.controller.rest;
 
 import com.github.PopovDmitry.nstu.webcw.dto.AuthenticationRequestDTO;
+import com.github.PopovDmitry.nstu.webcw.model.User;
+import com.github.PopovDmitry.nstu.webcw.security.JwtTokenProvider;
+import com.github.PopovDmitry.nstu.webcw.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,14 +18,39 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/dev/api/public/auth")
 public class AuthenticationRestController {
 
-    @PostMapping("login")
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthenticationRestController(AuthenticationManager authenticationManager, UserService userService, JwtTokenProvider jwtTokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.userService = userService;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthenticationRequestDTO authenticationRequestDTO) {
-        return null;
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    authenticationRequestDTO.getEmail(), authenticationRequestDTO.getPassword()));
+            User user = userService.getUser(authenticationRequestDTO.getEmail()).orElseThrow(() ->
+                    new UsernameNotFoundException("User is not found"));
+            String token = jwtTokenProvider.createToken(authenticationRequestDTO.getEmail(), user.getRole().name());
+            Map<Object, Object> response = new HashMap<>();
+            response.put("email", authenticationRequestDTO.getEmail());
+            response.put("token", token);
+            return ResponseEntity.ok(response);
+        }
+        catch (AuthenticationException exception) {
+            return new ResponseEntity<>("Invalid email/password combination", HttpStatus.FORBIDDEN);
+        }
     }
 
     @PostMapping("/logout")
