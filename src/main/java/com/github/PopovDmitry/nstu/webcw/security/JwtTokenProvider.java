547df4239d612996e2bd3cc051a6dabb76bca6f6
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-04-14 17:17:25 +0700

    [STUDY-69] Added UserDetails implementation

--- src/main/java/com/github/PopovDmitry/nstu/webcw/security/JwtTokenProvider.java
+++ src/main/java/com/github/PopovDmitry/nstu/webcw/security/JwtTokenProvider.java
@@ -3,8 +3,11 @@ package com.github.PopovDmitry.nstu.webcw.security;
 import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.Base64;
 import java.util.Date;
 
 @Component
@@ -15,6 +18,11 @@ public class JwtTokenProvider {
 
     @Value("${jwt.expiration}")
     private long validityInMilliseconds;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
 
     public String createToken(String email, String role) {
         Claims claims = Jwts.claims().setSubject(email);
@@ -38,6 +46,13 @@ public class JwtTokenProvider {
         catch (JwtException | IllegalArgumentException exception) {
             throw new JwtAuthenticationException("JWT token is expired or invalid", HttpStatus.UNAUTHORIZED);
         }
+    }
 
+    public Authentication getAuthentication(String token) {
+
+    }
+
+    public String getEmail(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
     }
 }
---
+++ src/main/java/com/github/PopovDmitry/nstu/webcw/security/SecurityUser.java
@@ -0,0 +1,58 @@
+package com.github.PopovDmitry.nstu.webcw.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class SecurityUser implements UserDetails {
+
+    private final String username;
+    private final String password;
+    private final List<SimpleGrantedAuthority> authorities;
+    private final boolean isActive;
+
+    public SecurityUser(String username, String password, List<SimpleGrantedAuthority> authorities, boolean isActive) {
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+        this.isActive = isActive;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+}
