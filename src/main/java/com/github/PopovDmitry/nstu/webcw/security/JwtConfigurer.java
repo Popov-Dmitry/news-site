18e670f5d3583bc8db846af684d4bfb189732d0f
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-04-15 01:23:17 +0700

    [STUDY-69] Added JwtConfigurer

---
+++ src/main/java/com/github/PopovDmitry/nstu/webcw/security/JwtConfigurer.java
@@ -0,0 +1,24 @@
+package com.github.PopovDmitry.nstu.webcw.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final JwtTokenFilter jwtTokenFilter;
+
+    @Autowired
+    public JwtConfigurer(JwtTokenFilter jwtTokenFilter) {
+        this.jwtTokenFilter = jwtTokenFilter;
+    }
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
