284d93ce169b9cacca434c48fedb1bbf84b3f223
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-04-19 00:49:32 +0700

    [STUDY-69] Added more validation for user

--- src/main/java/com/github/PopovDmitry/nstu/webcw/model/User.java
+++ src/main/java/com/github/PopovDmitry/nstu/webcw/model/User.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +26,7 @@ public class User {
 
     @Column(name = "first_name")
     @NotEmpty(message = "Имя не может быть пустым")
+    @Size(min = 2, max = 64, message = "Длина имени может быть от 2 до 64 символов")
     private String firstName;
 
     @Column(name = "last_name")
@@ -31,11 +34,13 @@ public class User {
 
     @Column(name = "email")
     @NotEmpty(message = "Email не может быть пустым")
+    @Email(message = "Неверный формат имейла")
+    @Size(min = 2, max = 255, message = "Некорректная длина имейла")
     private String email;
 
     @Column(name = "password")
     @NotEmpty(message = "Пароль не может быть пустым")
-    @Min(value = 5, message = "Пароль должен состоять от 5 символов")
+    @Size(min = 5, max = 255, message = "Пароль должен состоять от 5 символов")
     private String password;
 
     @Column(name = "role")
